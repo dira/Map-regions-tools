6800a6d0636f9b395c83af13776c23ffe7f54932
Irina Dumitrascu <irina.dumitrascu@2gmail.com>
2019-10-12 15:59:07 +0300

    Refactor - wait for both data sources before loading
    so we can combine info

--- index_files/scripts.js
+++ index_files/scripts.js
@@ -20,11 +20,34 @@ function showMarkers(mapLayer, locations) {		// Add markers to the map
   }
 }
 
+function showCounties(json) {
+  function onEachFeature(feature, layer) {
+    var popupContent = feature.properties.name;
+    layer.bindPopup(popupContent);
+  }
+  var colors = ['#8c510a','#bf812d','#dfc27d','#f6e8c3','#c7eae5','#80cdc1','#35978f','#01665e']
+  for (var id in json.features) {
+    var county = json.features[id];
+    L.geoJSON([county], {
+      style: function (feature) {
+        return {
+          weight: 1,
+          color: "#666",
+          fillColor: colors[county.properties.regionId - 1],
+          opacity: 1,
+          fillOpacity: 0.8
+        }
+      },
+      onEachFeature: onEachFeature,
+    }).addTo(map);
+  }
+};
+
 function showCenters(mapLayer, locations) {   // Add markers to the map
   var icon;
   var marker;
   for (var key in locations) {
-    var iconName = `<div class="icon-text-county">`+locations[key]['judet']+`</div>`;
+    var iconName = `<div class="icon-text-county">`+locations[key]['name']+`</div>`;
     icon = new L.DivIcon({
       className: 'icon-div',
       popupAnchor: [-6,-42],
@@ -35,12 +58,23 @@ function showCenters(mapLayer, locations) {   // Add markers to the map
     });
 
     var marker = L.marker([locations[key]['lat'], locations[key]['lon']],{
-      title: locations[key]['judet'],
+      title: locations[key]['name'],
       opacity: mapData.markerOpacity,
       icon: icon
     }).bindPopup('centrul').addTo(mapLayer);
   }
 }
+
+function getCenters(countours) {
+  var county_centers = []
+  for (var id in countours.features) {
+    var county = countours.features[id];
+
+    var center = getPolygonCenter(county.geometry.coordinates[0]);
+    county_centers.push({lon: center[0], lat: center[1], name: county.properties.name, code: county.properties.mnemonic});
+  }
+  return county_centers;
+};
 
 function getPolygonCenter(coordinates) {
   var n = coordinates.length;
