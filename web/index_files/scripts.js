c4e12b9e49a49ffd1954af0456d9422f71dd3b84
Irina Dumitrascu <irina.dumitrascu@2gmail.com>
2019-10-13 15:22:55 +0300

    Different color for each county

--- web/index_files/scripts.js
+++ web/index_files/scripts.js
@@ -14,15 +14,26 @@ function showCounties(json) {
     var popupContent = feature.properties.name;
     layer.bindPopup(popupContent);
   }
-  var colors = ['#8c510a','#bf812d','#dfc27d','#f6e8c3','#c7eae5','#80cdc1','#35978f','#01665e']
+  var blue = ["#003c30", "#01665e", "#35978f", "#80cdc1", "#c7eae5", "#f5f5f5"]
+  var brown = ["#402404", "#f6e8c3", "#dfc27d", "#bf812d", "#8c510a", "#543005"]
+  var orange = ["#cc4c02", "#ec7014", "#fe9929", "#fec44f", "#fee391", "#fff7bc", "#ffffe5"]
+  var region_colors = [ orange, blue, orange, blue, orange, blue, brown, blue ]
+  var region_pointers = {}
   for (var id in json.features) {
     var county = json.features[id];
+    region_index = county.properties.regionId - 1
+    if (region_pointers[region_index] >= 0) {
+      region_pointers[region_index] = region_pointers[region_index] + 1
+    } else {
+      region_pointers[region_index] = 0
+    }
+    color = region_colors[region_index][region_pointers[region_index]];
     L.geoJSON([county], {
       style: function (feature) {
         return {
           weight: 1,
           color: "#666",
-          fillColor: colors[county.properties.regionId - 1],
+          fillColor: color,
           opacity: 1,
           fillOpacity: 0.8
         }
